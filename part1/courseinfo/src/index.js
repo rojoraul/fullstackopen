a31dfeed972e68d1d39264619fe845d83cb99f9d
Raúl Rojo <raulrojo98@gmail.com>
2022-08-17 00:05:06 +0200

    Exercises 1.1-1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
 
 const Title = (props) => {
 
@@ -57,4 +59,6 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(rootElement);
+
+root.render(<App />);
