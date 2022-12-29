746725c217fa6068688c5972e9125a51fa1db78c
rojoraul <raulrojo98@gmail.com>
2022-12-29 21:48:43 +0100

    Exercise 4.4

--- part4/blogs/tests/list_helper.test.js
+++ part4/blogs/tests/list_helper.test.js
@@ -1,8 +1,62 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+const initialBlogs = [
+    { 
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    { 
+        _id: '5a422aa71b54a676234d37f8',
+        title: 'Go To Statement Considered Harmful 2',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 7,
+        __v: 0
+    },
+    { 
+        _id: '1422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful 3',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 10,
+        __v: 0
+    }
+]
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        const blogs = []
+        
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        const blogs = [initialBlogs[0]]
+        
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(blogs[0].likes)
+    })
+
+    test('of a bigger list is calculated correctly', () => {
+        const result = listHelper.totalLikes(initialBlogs)
+        let sum = 0;
+
+        initialBlogs.forEach(blog => {
+            sum += blog.likes
+        })
+        
+        expect(result).toBe(sum)
+    })    
 })
+
+test('dummy returns one', () => {
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
--- part4/blogs/utils/list_helper.js
+++ part4/blogs/utils/list_helper.js
@@ -1,7 +1,20 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    let sum = 0
+
+    blogs.forEach(blog => {
+        sum += blog.likes
+    });
+
+    return sum
+}
+
+
   
-  module.exports = {
-    dummy
+module.exports = {
+    dummy,
+    totalLikes
   }
